efe3cfd397d1bd181b1ef0ab31741bbb586429b7
Daniel Haase <git@dhaase.de>
2017-02-10 23:51:06 +0100

    added Python implementation

---
+++ computation/python/main.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def usage():
+    pass
+
+
+def fingerprint():
+    pass
+
+
+def shuffle(nStart, nEnd, nBy):
+    for n in range(nStart, nEnd + 1, nBy):
+        twoNPlusOne = 2 * n + 1
+        s = 1
+        k = 2
+        while (k != 1):
+            s += 1
+            if 2 <= k <= n:
+                k *= 2
+            elif k > n:
+                k = 2 * k - twoNPlusOne
+            else:
+                break
+        print(str(n) + " " + str(s) + " ")
+
+
+def main():
+    argv = sys.argv[1:]
+    argc = len(argv)
+    if argc == 1:
+        shuffle(int(argv[0]), int(argv[0]), 1)
+    elif argc == 2:
+        shuffle(int(argv[0]), int(argv[1]), 1)
+    elif argc == 3:
+        shuffle(int(argv[0]), int(argv[1]), int(argv[2]))
+
+
+if __name__ == "__main__":
+    main()
